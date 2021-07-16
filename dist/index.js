fc99366b63ca77a14593528d2468d2dd3dc85c94
Francisco Maseda <frmadem@gmail.com>
2021-07-16 17:14:54 +0000

    REcompile

--- dist/index.js
+++ dist/index.js
@@ -7452,7 +7452,7 @@ module.exports = function(action_type, ctx){
  
       core.info(JSON.stringify(rr, null, 4))
 
-      return rr.filter(r => r.data.prerelease)[0]
+   //   return rr.filter(r => r.data.prerelease)[0]
 
     }).then((r) => {
     
