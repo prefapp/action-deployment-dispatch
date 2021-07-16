1ac0c941c9c347a6c0cbf987f8a4ea15e2ec5c04
Francisco Maseda <frmadem@gmail.com>
2021-07-16 15:28:15 +0000

    Add initial index implementation

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const github = require('@actions/github');
 
 async function run(){
 
+  const ctx = {
+   
+    token: core.getInput('token'),
+   
+    repository: core.getInput('repository'),
+   
+    eventType: core.getInput('event-type'),
+   
+    clientPayload: core.getInput('client-payload')
+  
+  }
 
+  
 }
 
 run()
