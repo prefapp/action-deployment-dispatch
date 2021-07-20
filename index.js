3506f5849519f078b54a28f6ab520dbc8532043a
Francisco Maseda <frmadem@gmail.com>
2021-07-20 11:04:23 +0000

    Fix event name

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 const github = require('@actions/github');
 
+const Deployment = require("./utils/Deployment.js")
 const ImagesCalculator = require("./utils/ImagesCalculator.js")
 const GitControl = require("./utils/GitControl.js")
 
+const fs = require("fs")
+
 async function run(){
 
+  //
+  // We create a context to pass to other parts of the system
+  //
   const ctx = {
 
     github_token: core.getInput("github_token"),
@@ -23,36 +29,102 @@ async function run(){
 
     pull_request: core.getInput("pull_request"),
 
-    deployment_file: core.getInput("deployment_file")
+    deployment_file: core.getInput("deployment_file"),
+
+    triggered_event: core.getInput("triggered_event"),
   
   }
 
-  core.info("Loading")
+  core.info(JSON.stringify(ctx, null, 4))
+
+  //
+  // we check if there were changes on the deployments file. 
+  // If that is the case, we dispatch ALL its content
+  //
+  if( ctx.pull_request ){
   
-  core.info(`Repo ${ctx.state_repo}`)
+    const deploymentFileHasChanges = await new GitControl({ctx}).deploymentHasChanges()
   
-  let info = await ImagesCalculator("last_release", ctx)
+    if( deploymentHasChanges ) {
 
-  core.info("Latest release " + info)
+      return __processDeploymentFileWithChanges(ctx)
+    
+    }
+  }
 
-  info = await ImagesCalculator("last_prerelease", ctx)
+  //
+  // We process the normal event
+  //
+  return processEvent(ctx)
 
-  core.info("Latest prerelease " + info)
+  //core.info("Loading")
+  //
+  //core.info(`Repo ${ctx.state_repo}`)
+  //
+  //let info = await ImagesCalculator("last_release", ctx)
 
-  info = await ImagesCalculator("branch_main", ctx)
+  //core.info("Latest release " + info)
 
-  core.info("commit " + info)
+  //info = await ImagesCalculator("last_prerelease", ctx)
 
-  info = await ImagesCalculator("branch_branch2", ctx)
+  //core.info("Latest prerelease " + info)
 
-  core.info("commit " + info)
+  //info = await ImagesCalculator("branch_main", ctx)
 
-  const changes = await new GitControl({ctx}).deploymentHasChanges()
+  //core.info("commit " + info)
 
-  if( changes )
-    core.info("The file of deployments has changed!!")
-  else
-    core.info("The file of deployments has not changed")
+  //info = await ImagesCalculator("branch_branch2", ctx)
+
+  //core.info("commit " + info)
+
+  //const changes = await new GitControl({ctx}).deploymentHasChanges()
+
+  //if( changes )
+  //  core.info("The file of deployments has changed!!")
+  //else
+  //  core.info("The file of deployments has not changed")
 }
 
+  function processDeploymentFileWithChanges(ctx){
+  
+  }
+  
+  function processEvent(ctx){
+   
+    // load the deployments
+    const deployment = loadDeployment(ctx)
+
+    // get changes based on type of trigger
+    let changes = false
+
+    switch(ctx.triggered_event){
+
+      case "prerelease":
+        changes = deployment.parse("last_prerelease")
+        break
+
+      case "release":
+        changes = deployment.parse("last_release")
+        break
+
+      default: 
+        //we take the branch
+        if( ctx.triggered_event == "pull_request")
+          changes = ""
+
+      core.info(JSON.stringify(changes, null, 4))
+        
+    }
+
+
+  }
+
+    //
+    // Loads the deployments file (as it is defined on inputs.deployment_file)
+    //
+    function loadDeployment(ctx){
+
+      return new Deployment( fs.readFileSync( ctx.deployment_file ) ).init()
+    }
+  
 run()
