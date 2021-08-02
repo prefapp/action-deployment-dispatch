73bd20e8542c98dd652834f1412be99d06614fea
Francisco Maseda <frmadem@gmail.com>
2021-08-02 15:49:35 +0000

    Compile

--- dist/index.js
+++ dist/index.js
@@ -12236,7 +12236,7 @@ async function run(){
   
   }
 
-  core.info(JSON.stringify(ctx, null, 4))
+  core.info(JSON.stringify(github.context, null, 4))
 
   //
   // we check if there were changes on the deployments file. 
