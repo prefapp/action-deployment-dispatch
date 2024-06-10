0c31a4d94342679002f0470c34f7f680fa250653
Jose Alvarez <jose.alvarez@prefapp.es>
2024-06-10 18:41:39 +0200

    Update eslint config

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es6: true,
+        jest: true,
+        node: true
+    },
+    extends: "eslint:recommended",
+    globals: {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    parserOptions: {
+        ecmaVersion: 2018
+    },
+    rules: {
+        "no-unused-vars": 1
+    },
+    ignorePatterns: ["dist/*"],
+}
